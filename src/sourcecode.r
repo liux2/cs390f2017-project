91bc45c556a3f06e8227da4cd2b1d29c6a929a7a
CMPSCisGOD <freezeeyong@hotmail.com>
2017-11-15 22:41:23 -0500

    Update sourcecode.r

--- src/sourcecode.r
+++ src/sourcecode.r
@@ -1,13 +1,6 @@
 install.packages("PBSmapping")
 install.packages("maptools")
-install.packages("lubridate")
-install.packages("PBSmapping")
-install.packages("maptools")
-install.packages("data.table")
-
 library(tidyverse)
-library(lubridate)
-library(data.table)
 library(psych)
 library(PBSmapping)
 library(maptools)
@@ -15,10 +8,16 @@ library(maptools)
 #Import US map.
 usShape <- importShapefile( file.choose(),readDBF=TRUE)
 # File to load: gz_2010_us_040_00_500k.shp
+pointPlot <- function(Y, X, EID)
+{
+pointData <- data.frame(EID,X,Y)
+eventData <- as.EventData(pointData,projection=NA)
+addPoints(eventData,col="green",cex=.5)
+}
 
 #We will be using ver.2
 MassShoot <- file.choose()
 MassShooting <- read.csv(MassShoot)
 
-#data pre-handling
+#data pre-handing
 dat <- MessShooting %>% 
