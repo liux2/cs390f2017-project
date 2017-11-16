06b34bee2236d2523fa9f942fd76c2a70ef2e308
CMPSCisGOD <freezeeyong@hotmail.com>
2017-11-15 22:10:59 -0500

    Update sourcecode.r

--- src/sourcecode.r
+++ src/sourcecode.r
@@ -1,5 +1,13 @@
+install.packages("PBSmapping")
+install.packages("maptools")
 library(tidyverse)
 library(psych)
+library(PBSmapping)
+library(maptools)
+
+#Import US map.
+usShape <- importShapefile( file.choose(),readDBF=TRUE)
+# File to load: gz_2010_us_040_00_500k.shp
 
 #We will be using ver.2
 MassShoot <- file.choose()
