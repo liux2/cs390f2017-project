bc05b510a73adaa7242fe9b502298f369230030a
Ziyang Zhang <zhangz3@aldenv131.allegheny.edu>
2017-11-16 20:15:30 -0500

    changes

--- src/sourcecode.r
+++ src/sourcecode.r
@@ -6,15 +6,16 @@ library(PBSmapping)
 library(maptools)
 
 #Import US map.
-usShape <- importShapefile( file.choose(),readDBF=TRUE)
+usShape <- importShapefile(file.choose(),readDBF=TRUE)
 # File to load: gz_2010_us_040_00_500k.shp
 pointPlot <- function(Y, X, EID)
 {
 pointData <- data.frame(EID,X,Y)
 eventData <- as.EventData(pointData,projection=NA)
-addPoints(eventData,col="green",cex=.5)
+addPoints(eventData,col="red",cex=.5)
 }
-
+plotPolys(usShape)
+plotPolys(usShape, xlim=c(-130,-60),ylim=c(20,50))
 #We will be using ver.2
 MassShoot <- file.choose()
 MassShooting <- read.csv(MassShoot)
