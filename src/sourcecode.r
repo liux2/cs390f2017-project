d537a33e777e89d89a283c5c174659d6b06db863
Xingbang Liu <liux2@allegheny.edu>
2017-11-16 21:24:40 -0500

    handle

--- src/sourcecode.r
+++ src/sourcecode.r
@@ -17,17 +17,25 @@ library(lubridate)
 MassShoot <- file.choose()
 MassShooting <- read.csv(MassShoot)
 
-#data pre-handing
-MassShooting<- rename(MassShooting,ID=`S#`,Total=`Total victims`,Mental=`Mental Health Issues`)
+#data pre-handing(part learned from Shuhao, Linkedin: https://www.linkedin.com/in/%E4%B9%A6%E8%B1%AA-%E9%82%AC-57891910b/)
+MassShooting <- rename(MassShooting,ID=`S.`,Total=`Total.victims`,Mental=`Mental.Health.Issues`)
+
+#add vegas data
+MassShooting$Injured[MassShooting$ID==1] <- 527
+MassShooting$Fatalities[MassShooting$ID==1] <- 59
+MassShooting$Total[MassShooting$ID==1] <- 586
+MassShooting$Race[MassShooting$ID==1] <- "White"
+MassShooting$Gender[MassShooting$ID==1] <- "Male"
+MassShooting$Summary[MassShooting$ID==1] <- "Stephen Paddock, a 64-year-old gunman opened fire on a large crowd of concertgoers at the Route 91 Harvest music festival on the Las Vegas Strip, killing 59 people and injuring 527"
 
 MassShooting$Gender[MassShooting$Gender=="M"] <- "Male"
 MassShooting$Gender[MassShooting$Gender=="M/F"] <- "Male/Female"
 
-MassShooting$Mental[MassShooting$Mental=="Unclear" | MassShooting$Mental=="unknown"] <- "NA"
+MassShooting$Mental[MassShooting$Mental=="Unclear" | MassShooting$Mental=="unknown" | MassShooting$Mental=="Unknown"] <- "Unknown"
 
 MassShooting$Race[MassShooting$Race=="black" | MassShooting$Race=="Black American or African American" | MassShooting$Race=="Black American or African American/Unknown"] <- "Black"
 MassShooting$Race[MassShooting$Race=="white" | MassShooting$Race=="White American or European American" | MassShooting$Race=="White American or European American/Some other Race"] <- "White"
-MassShooting$Race[MassShootingt$Race=="unclear" | MassShooting$Race==""] <- "NA"
+MassShooting$Race[MassShooting$Race=="unclear" | MassShooting$Race==""] <- "Unknown"
 MassShooting$Race[MassShooting$Race=="Asian American/Some other race" | MassShooting$Race=="Asian American"] <- "Asian"
 MassShooting$Race[MassShooting$Race=="Latino" | MassShooting$Race=="Native American" | MassShooting$Race=="Native American or Alaska Native" | MassShooting$Race=="Some other race" | MassShooting$Race=="Two or more races"] <- "Other"
 
