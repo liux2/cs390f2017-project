f2cf4e6c2585e03dee255653c411e41ab680a736
Xingbang Liu <liux2@allegheny.edu>
2017-12-08 21:16:52 -0500

    project update

--- src/sourcecode.r
+++ src/sourcecode.r
@@ -16,14 +16,6 @@ MassShooting <- read.csv(MassShoot)
 #data pre-handing(part learned from Shuhao, Linkedin: https://www.linkedin.com/in/%E4%B9%A6%E8%B1%AA-%E9%82%AC-57891910b/)
 MassShooting <- rename(MassShooting,state= `State`,ID=`S.`,Total=`Total.victims`,Mental=`Mental.Health.Issues`)
 
-#add vegas data
-MassShooting$Injured[MassShooting$ID==1] <- 527
-MassShooting$Fatalities[MassShooting$ID==1] <- 59
-MassShooting$Total[MassShooting$ID==1] <- 586
-MassShooting$Race[MassShooting$ID==1] <- "White"
-MassShooting$Gender[MassShooting$ID==1] <- "Male"
-MassShooting$Summary[MassShooting$ID==1] <- "Stephen Paddock, a 64-year-old gunman opened fire on a large crowd of concertgoers at the Route 91 Harvest music festival on the Las Vegas Strip, killing 59 people and injuring 527"
-
 #keep handling
 MassShooting$Gender[MassShooting$Gender=="M"] <- "Male"
 MassShooting$Gender[MassShooting$Gender=="M/F"] <- "Male/Female"
@@ -60,12 +52,13 @@ states <- ddply(states_map, .(region), summarise, lat = mean(lat, na.rm = TRUE),
 Deathrate<- merge(Deathrate, states, by.x = "State", by.y = "region")
 ratemap <- ggplot(Deathrate, aes(map_id = State, fill = rate_q)) + geom_map(map = states_map, colour = "black") + scale_fill_brewer(palette = "Set2") + expand_limits(x = states_map$long, y = states_map$lat)+ coord_map("polyconic") + labs(fill = "Death Rate\nPercentile", title = "MassShooting in USA")
 
-#xiazijun de 
+#xiazijun de
 #Shooting_state <- filter(MassShooting, state %in% c(" texas", " florida"))
 #Shooting_state <- select(Shooting_state, state, Total, year)
 Shooting_states <- file.choose()
 Shooting_state <- read.csv(Shooting_states)
 t.test(data = Shooting_state, victim ~ state)
+
 #age from summary
 temp <- mutate(MassShooting,age=str_extract_all(MassShooting$Summary,pattern="(,\\s)\\d{2}(,)"),age2=str_extract_all(MassShooting$Summary,pattern="(a\\s)\\d{2}(-year)"))
 temp$age <- str_sub(temp$age,3,4)
@@ -81,3 +74,15 @@ tem <- arrange(tem,ID)
 tem$age <- as.integer(tem$age)
 tem3 <- tem %>% select(ID,age) %>% mutate(agecut=cut(tem$age,breaks = 10*(1:7)))
 shoot_age <- left_join(tem3, MassShooting,by="ID")
+
+#plot of age 
+
+ggplot(shoot_age, mapping = aes(agecut))+geom_bar(fill="lightblue")+ggtitle("age distribution")
+ggplot(shoot_age, mapping=aes(agecut,fill=Mental))+geom_bar(position="fill")+ggtitle("age with mental factor")
+
+#plot of mental statement
+mental <- subset(MassShooting, Mental != "Unknown")
+#mental_Cas <- data.frame(table(mental$Mental,mental$Level))
+#colnames(mental_Cas) <- c("Mental","Level","Freq")
+#ggplot(mental_Cas, mapping = aes(x= factor(Level, levels=c("Under 5","5 to 10","above 10")),y= Freq,fill = Mental))+geom_bar(stat="identity",position="dodge")+xlab("Level")
+barplot(tapply(mental$Total,mental$Mental,sum),main="Shooter with mental disease",col = "lightblue",ylab = "Number of casualties")
